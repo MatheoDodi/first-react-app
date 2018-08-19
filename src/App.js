a64eb0ae2739046ad385640d7169b330a881117e
MatthewDodi <matthew.dodi@gmail.com>
2018-08-19 13:28:24 -0700

    insted of using a ternary operator for a conditional statement, created a variable that holds all the JSX in it, and depending on weather a flag is true or false that variable is displayed

--- src/App.js
+++ src/App.js
@@ -48,6 +48,22 @@ class App extends Component {
       cursor : 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age}
+              click={this.switchNameHandler.bind(this, 'Mat')}
+              changed={this.nameChangedHandler}> hobbies: coding</Person>
+            <Person name={this.state.persons[1].name}
+              age={this.state.persons[1].age} />
+        </div>
+      );
+    }
+
 
     return (
       <div className="App">
@@ -56,18 +72,7 @@ class App extends Component {
         <button
         style={style}
         onClick={this.togglePersonsHandler}>Show Persons</button>
-
-        { this.state.showPersons ? 
-          <div >
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}
-              click={this.switchNameHandler.bind(this, 'Mat')}
-              changed={this.nameChangedHandler}> hobbies: coding</Person>
-            <Person name={this.state.persons[1].name}
-              age={this.state.persons[1].age} />
-          </div> : null
-        }
+        {persons}
       </div>
     );
   
