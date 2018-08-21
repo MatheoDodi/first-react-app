f93141671e504035965307522dd71b857030ed39
MatthewDodi <matthew.dodi@gmail.com>
2018-08-21 13:06:06 -0700

    converted all stateless components in to statefull components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Person.css'
 
-const person = ( props ) => {
-    return (
-    <div onClick={props.click} className='Person'>
-        <p>I'm {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-)
-};
+class Person extends Component {
+    render() {
+        return (
+            <div onClick={this.props.click} className='Person'>
+                <p>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        )
+    }
+}
 
-export default person;
+export default Person;
